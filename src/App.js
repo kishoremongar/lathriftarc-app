38ff6727ab495ec39866ac3827b4908a6035344f
Kishore Mongar <84139548+fushigina-Shinobi@users.noreply.github.com>
2022-09-15 22:35:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline header">Hello world!</h1>
+      <Button variant="contained">Hello World</Button>
+    </>
+  );
+}
+
+export default App;
